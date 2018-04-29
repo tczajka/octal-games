60a1df7794cb470abb3810a00b25634a5b7f8219
Tomek Czajka <tczajka@gmail.com>
2018-04-28 23:02:54 -0700

    fix mex overflow bug for huge vectors

--- src/simple_solver.cc
+++ src/simple_solver.cc
@@ -3,14 +3,14 @@
 #include <stdexcept>
 
 std::uint32_t mex(const std::vector<std::uint32_t> &v) {
+  if (v.size() >= std::numeric_limits<std::uint32_t>::max()) {
+    throw std::domain_error("too many mex params");
+  }
   std::vector<bool> seen(v.size() + 1u, false);
   for (auto x : v) {
     if (x < seen.size()) seen[x] = true;
   }
   size_t res = 0;
   while (seen[res]) ++res;
-  if (res > std::numeric_limits<std::uint32_t>::max()) {
-    throw std::domain_error("nim value too large");
-  }
   return res;
 }
